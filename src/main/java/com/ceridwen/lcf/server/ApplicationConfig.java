552278cbad9c1fdbd38f5ac8b71192530d3f4a9a
Matthew J. DOvey <m.dovey@ceridwen.com>
2019-05-22 08:06:35 +0100

    Stripped down webservice

---
+++ src/main/java/com/ceridwen/lcf/server/ApplicationConfig.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ceridwen.lcf.server;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Matthew
+ */
+@javax.ws.rs.ApplicationPath("lcf/1.0")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+    }
+    
+}
