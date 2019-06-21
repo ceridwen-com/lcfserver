2eb9033db884fb1bd1402714e040d3b42a008006
Matthew J. Dovey <m.dovey@ceridwen.com>
2019-06-21 21:37:09 +0100

    Update Authenticator to permit patron-id validation and superuser validation

--- lcfserver-resources-memory/src/main/java/com/ceridwen/lcf/server/resources/memory/database/Authenticator.java
+++ lcfserver-resources-memory/src/main/java/com/ceridwen/lcf/server/resources/memory/database/Authenticator.java
@@ -20,17 +20,18 @@ import com.ceridwen.lcf.model.authentication.AuthenticationToken;
 import com.ceridwen.lcf.model.authentication.BasicAuthenticationToken;
 import com.ceridwen.lcf.model.exceptions.EXC02_InvalidUserCredentials;
 import com.ceridwen.lcf.model.exceptions.EXC03_InvalidTerminalCredentials;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
-import org.bic.ns.lcf.v1_0.Authorisation;
-import org.bic.ns.lcf.v1_0.EntityType;
-import org.bic.ns.lcf.v1_0.LcfEntity;
+import org.bic.ns.lcf.v1_0.Patron;
 
 /**
  *
@@ -39,7 +40,8 @@ import org.bic.ns.lcf.v1_0.LcfEntity;
 public class Authenticator {
     Map<EntityType, Map<Operation, List<AuthenticationCategory>>> acls = new EnumMap<>(EntityType.class);
     Map<String, String> terminalAccounts = new HashMap<>();
-    Map<String, String> userAccounts =new HashMap<>();
+    Map<String, String> userAccounts = new HashMap<>();
+    List<String> superusers = new ArrayList<>();
     
     static Authenticator authenticator = new Authenticator();
     
@@ -56,22 +58,40 @@ public class Authenticator {
     
     public void Config() {
         Logger.getLogger(Authenticator.class.getName()).info("Loading Default ACLS Data");
-        updatePassword(AuthenticationCategory.TERMINAL, "terminal", "password");     
+        updatePassword(AuthenticationCategory.TERMINAL, "terminal", "password");  
+        updateSuperUserPassword("root", "password");
         
         addACL(Operation.DELETE, AuthenticationCategory.TERMINAL);
         addACL(Operation.POST, AuthenticationCategory.TERMINAL);
         addACL(Operation.PUT, AuthenticationCategory.TERMINAL);
         removeACL(EntityTypes.Type.Patron, Operation.PUT, AuthenticationCategory.TERMINAL);
-        
+        removeACL(EntityTypes.Type.Loan, AuthenticationCategory.TERMINAL);
+        removeACL(EntityTypes.Type.Reservation, AuthenticationCategory.TERMINAL);
+        addACL(EntityTypes.Type.Patron, Operation.PUT, AuthenticationCategory.USER);
+        addACL(EntityTypes.Type.Patron, Operation.GET, AuthenticationCategory.USER);
+        addACL(EntityTypes.Type.Patron, Operation.UPDATE, AuthenticationCategory.USER);
+        addACL(EntityTypes.Type.Loan, AuthenticationCategory.USER);
+        addACL(EntityTypes.Type.Reservation, AuthenticationCategory.USER);
+        addACL(EntityTypes.Type.Charge, AuthenticationCategory.USER);
+       
         Logger.getLogger(Authenticator.class.getName()).info("ACLS Data Load Complete");
     }
 
-
-    
     public void updatePassword(AuthenticationCategory category, String id, String password) {
+        updatePassword(category, id, password, false);
+    }
+
+    public void updateSuperUserPassword(String id, String password) {
+        updatePassword(AuthenticationCategory.USER, id, password, true);
+    }
+      
+    private void updatePassword(AuthenticationCategory category, String id, String password, boolean superuser) {
         switch (category) {
             case USER:
                 userAccounts.put(id, password);
+                if (superuser) {
+                    superusers.add(id);
+                }
                 break;
             case TERMINAL:
                 terminalAccounts.put(id, password);
@@ -92,20 +112,20 @@ public class Authenticator {
         return false;
     }
 
-    public void authenticate(EntityType type, Operation operation, List<AuthenticationToken> tokens) {
+    public void authenticate(EntityTypes.Type type, Operation operation, List<AuthenticationToken> tokens, Object entity) {
         if (acls.containsKey(type)) {
             Map<Operation, List<AuthenticationCategory>> ops = acls.get(type);
             if (ops.containsKey(operation)) {
                 List<AuthenticationCategory> categories = ops.get(operation);
                 categories.sort(Comparator.comparing(AuthenticationCategory::ordinal));
                 for (AuthenticationCategory category: categories) {
-                    authenticate(category, tokens);
+                    authenticate(category, tokens, entity);
                 }
             }
         }
     }
 
-    public void authenticate(AuthenticationCategory category, List<AuthenticationToken> tokens) {
+    public void authenticate(AuthenticationCategory category, List<AuthenticationToken> tokens, Object entity) {
         switch (category) {
             case TERMINAL:
                 if (!authenticate(terminalAccounts, tokens.stream().filter(
@@ -115,28 +135,38 @@ public class Authenticator {
                 }
                 break;
             case USER:
+                String barcode = getPatronBarcode(entity);
                 if (!authenticate(userAccounts, tokens.stream().filter(
-                        token -> token.getAuthenticationCategory() == AuthenticationCategory.USER)
-                        .collect(Collectors.toList()))) {
+                        token -> (token.getAuthenticationCategory() == AuthenticationCategory.USER &&
+                                 (barcode == null || ((BasicAuthenticationToken)token).getUsername().equals(barcode)) || superusers.contains(((BasicAuthenticationToken)token).getUsername()))
+                        ).collect(Collectors.toList()))) {
                     throw new EXC02_InvalidUserCredentials("", "", "", null);
                 }
                 break;
         }
     }
     
-    public void authenticate(String patronId, List<AuthenticationToken> authTokens) {
+    private String getPatronBarcode(Object entity) {
         try {
-            Authenticator.getAuthenticator().authenticate(AuthenticationCategory.USER,  authTokens.stream().filter(
-                    token -> (token.getAuthenticationCategory() == AuthenticationCategory.USER &&
-                            ((BasicAuthenticationToken)token).getUsername().equals(patronId)))
-                    .collect(Collectors.toList()));
-        } catch (EXC02_InvalidUserCredentials ex1) {
-            try {
-                Authenticator.getAuthenticator().authenticate(AuthenticationCategory.TERMINAL, authTokens);
-            } catch (EXC03_InvalidTerminalCredentials ex2) {
-                throw ex1;
+            if (entity == null) {
+                return null;
             }
+            
+            if (entity instanceof Patron) {
+                return ((Patron)entity).getBarcodeId();
+            }
+            
+            Method method = entity.getClass().getMethod("getPatronRef");
+            Class returnType = method.getReturnType();
+            if (returnType.equals(String.class)) {
+                String ref = (String)method.invoke(entity);
+                Patron patron = (Patron)Database.getDatabase().get(EntityTypes.Type.Patron, ref);
+                return patron.getBarcodeId();
+            }
+        } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+            Logger.getLogger(Authenticator.class.getName()).log(Level.FINE, null, ex);
         }
+        return null;
     }
     
     public void addACL(EntityType type, Operation operation, AuthenticationCategory category) {
